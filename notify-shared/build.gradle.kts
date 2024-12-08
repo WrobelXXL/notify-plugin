b4ee78dfa0d9d7f649c9a36d9cff969bf1953fdf
Jannik <jannik.developer02@gmail.com>
2024-12-08 22:43:57 +0100

    add configurate and kyori adventure libraries

---
+++ notify-shared/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    api(libs.simplecloud.controller)
+    api("org.spongepowered:configurate-yaml:4.0.0")
+    api("org.spongepowered:configurate-extra-kotlin:4.1.2")
+
+    api("net.kyori:adventure-api:4.17.0")
+    api("net.kyori:adventure-text-minimessage:4.17.0")
+}
