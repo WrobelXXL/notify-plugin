e9cb45de61353575456ea6ae8e54a62b04e7b5ea
Jannik <jannik.developer02@gmail.com>
2024-12-09 23:11:43 +0100

    fix placeholders

--- notify-shared/src/main/kotlin/app/simplecloud/plugin/notify/shared/NotifyPlugin.kt
+++ notify-shared/src/main/kotlin/app/simplecloud/plugin/notify/shared/NotifyPlugin.kt
@@ -47,26 +47,26 @@ class NotifyPlugin(
             filter.forEach {
                 val message = miniMessage(
                     it.message,
-                    Placeholder.parsed("<server_ip>", serverAfter.serverIp ?: "N/A"),
-                    Placeholder.parsed("<server_port>", serverAfter.serverPort.toString()),
-                    Placeholder.parsed("<server_group>", serverAfter.groupName ?: "N/A"),
+                    Placeholder.parsed("server_ip", serverAfter.serverIp ?: "N/A"),
+                    Placeholder.parsed("server_port", serverAfter.serverPort.toString()),
+                    Placeholder.parsed("server_group", serverAfter.groupName ?: "N/A"),
 
-                    Placeholder.parsed("<server_uuid>", serverAfter.uniqueId),
-                    Placeholder.parsed("<server_id>", serverAfter.numericalId.toString()),
+                    Placeholder.parsed("server_uuid", serverAfter.uniqueId),
+                    Placeholder.parsed("server_id", serverAfter.numericalId.toString()),
 
                     Placeholder.parsed(
-                        "<server_create_date>",
+                        "server_create_date",
                         dateFormat.format(timeStampToLong(serverAfter.createdAt))
                     ),
 
                     Placeholder.parsed(
-                        "<server_update_date>",
+                        "server_update_date",
                         dateFormat.format(timeStampToLong(serverAfter.updatedAt))
                     ),
 
-                    Placeholder.parsed("<online_players>", serverAfter.playerCount.toString()),
-                    Placeholder.parsed("<max_players>", serverAfter.maxPlayers.toString()),
-                    Placeholder.parsed("<server_state>", serverState.name)
+                    Placeholder.parsed("online_players", serverAfter.playerCount.toString()),
+                    Placeholder.parsed("max_players", serverAfter.maxPlayers.toString()),
+                    Placeholder.parsed("server_state", serverState.name)
                 )
 
                 function(message, it.permission)
