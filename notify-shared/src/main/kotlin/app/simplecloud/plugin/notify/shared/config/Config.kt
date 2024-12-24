6a1ca6e8792b1725f73135ef7d2d1ebb2de732dc
Jannik <jannik.developer02@gmail.com>
2024-12-24 12:53:24 +0100

    add more pretty default config messages

--- notify-shared/src/main/kotlin/app/simplecloud/plugin/notify/shared/config/Config.kt
+++ notify-shared/src/main/kotlin/app/simplecloud/plugin/notify/shared/config/Config.kt
@@ -21,19 +21,37 @@ data class Config(
                 "<server_state> - The new state of the server"
     )
     val serverStateFilter: List<ServerStateChangedFilterEntry> = listOf(
-        ServerState.STARTING,
-        ServerState.AVAILABLE,
-        ServerState.STOPPING
-    ).map {
         ServerStateChangedFilterEntry(
-            it,
+            ServerState.STARTING,
             "<color:#38bdf8><bold>⚡</bold></color> <hover:show_text:'<color:#38bdf8><bold>⚡</bold></color> Information of <server_group> <server_id>\n" +
                     "   <color:#a3a3a3>Timestamp:</color> <color:#38bdf8><server_update_date></color>\n" +
                     "   <color:#a3a3a3>State:</color> <color:#38bdf8><server_state></color>\n" +
                     "   <color:#a3a3a3>Server-IP:</color> <color:#38bdf8><server_ip></color>\n" +
                     "   <color:#a3a3a3>Port:</color> <color:#38bdf8><server_port></color>\n" +
                     "   <color:#a3a3a3>Players:</color> <color:#38bdf8><online_players>/<max_players></color>'>" +
-                    "<color:white>Server <server_group> <server_id></hover> updated its state to <color:#fbbf24><server_state></color>."
-        )
-    }
+                    "<color:white>Server <server_group> <server_id></hover> is now <color:#fbbf24>starting</color>."
+        ),
+
+        ServerStateChangedFilterEntry(
+            ServerState.AVAILABLE,
+            "<color:#38bdf8><bold>⚡</bold></color> <hover:show_text:'<color:#38bdf8><bold>⚡</bold></color> Information of <server_group> <server_id>\n" +
+                    "   <color:#a3a3a3>Timestamp:</color> <color:#38bdf8><server_update_date></color>\n" +
+                    "   <color:#a3a3a3>State:</color> <color:#38bdf8><server_state></color>\n" +
+                    "   <color:#a3a3a3>Server-IP:</color> <color:#38bdf8><server_ip></color>\n" +
+                    "   <color:#a3a3a3>Port:</color> <color:#38bdf8><server_port></color>\n" +
+                    "   <color:#a3a3a3>Players:</color> <color:#38bdf8><online_players>/<max_players></color>'>" +
+                    "<color:white>Server <server_group> <server_id></hover> is now <color:#a3e635>online</color>."
+        ),
+
+        ServerStateChangedFilterEntry(
+            ServerState.STOPPING,
+            "<color:#38bdf8><bold>⚡</bold></color> <hover:show_text:'<color:#38bdf8><bold>⚡</bold></color> Information of <server_group> <server_id>\n" +
+                    "   <color:#a3a3a3>Timestamp:</color> <color:#38bdf8><server_update_date></color>\n" +
+                    "   <color:#a3a3a3>State:</color> <color:#38bdf8><server_state></color>\n" +
+                    "   <color:#a3a3a3>Server-IP:</color> <color:#38bdf8><server_ip></color>\n" +
+                    "   <color:#a3a3a3>Port:</color> <color:#38bdf8><server_port></color>\n" +
+                    "   <color:#a3a3a3>Players:</color> <color:#38bdf8><online_players>/<max_players></color>'>" +
+                    "<color:white>Server <server_group> <server_id></hover> is now <color:#dc2626>stopping</color>."
+        ),
+    )
 )
