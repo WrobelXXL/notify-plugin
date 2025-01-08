1cba1429fbd7dc75f8e5cd7bd6c55e94bc183256
Philipp <p.eistrach@gmail.com>
2025-01-08 21:44:26 +0100

    feat: modrinth publishing

--- notify-bungeecord/build.gradle.kts
+++ notify-bungeecord/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    alias(libs.plugins.minotaur)
+}
+
 dependencies {
     api(project(":notify-shared"))
     compileOnly(libs.bungeecord)
@@ -5,4 +9,29 @@ dependencies {
 
     implementation("net.kyori:adventure-api:4.17.0")
     implementation("net.kyori:adventure-text-minimessage:4.17.0")
+}
+
+modrinth {
+    token.set(project.findProperty("modrinthToken") as String? ?: System.getenv("MODRINTH_TOKEN"))
+    projectId.set("daugEY3o")
+    versionNumber.set(rootProject.version.toString())
+    versionType.set("beta")
+    uploadFile.set(tasks.shadowJar)
+    gameVersions.addAll(
+        "1.20",
+        "1.20.1",
+        "1.20.2",
+        "1.20.3",
+        "1.20.4",
+        "1.20.5",
+        "1.20.6",
+        "1.21",
+        "1.21.1",
+        "1.21.2",
+        "1.21.3",
+        "1.21.4"
+    )
+    loaders.add("bungeecord")
+    changelog.set("https://docs.simplecloud.app/changelog")
+    syncBodyFrom.set(rootProject.file("README.md").readText())
 }
